e846a1cf3c81e78c44c77e00d0a5506c3b11deed
MarioKiller44 <mescribanor1410@gmail.com>
2025-07-20 23:54:36 +0200

    Añadiendo modificaciones necesarias para la implementación

--- Java/PeluqueriaAPPCorteccinoEscritorio/src/styles/GestionProductosStyle.java
+++ Java/PeluqueriaAPPCorteccinoEscritorio/src/styles/GestionProductosStyle.java
@@ -1,20 +1,20 @@
 package styles;
 
-//import com.formdev.flatlaf.FlatLightLaf;
-//
-//public class GestionProductosStyle extends FlatLightLaf{
-//	public static final String NAME = "GestionProductosStyle";
-//
-//	public static boolean setup() {
-//		return setup( new GestionProductosStyle() );
-//	}
-//
-//	public static void installLafInfo() {
-//		installLafInfo( NAME, GestionProductosStyle.class );
-//	}
-//
-//	@Override
-//	public String getName() {
-//		return NAME;
-//	}
-//}
+import com.formdev.flatlaf.FlatLightLaf;
+
+public class GestionProductosStyle extends FlatLightLaf{
+public static final String NAME = "GestionProductosStyle";
+
+	public static boolean setup() {
+		return setup( new GestionProductosStyle() );
+	}
+
+	public static void installLafInfo() {
+		installLafInfo( NAME, GestionProductosStyle.class );
+	}
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+}
